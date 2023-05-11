7c6d264d234aff91c8c32baa3b55e98ec7330677
kt81 <ktsh81@gmail.com>
2023-05-11 07:24:37 +0000

    ahh

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use function strpos;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +12,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
-        // oh
-        // ahhhhhhh
+        $pos = strpos('foo', 'f');
     }
 
     /**
